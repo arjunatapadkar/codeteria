64f474a08331cd8dc6ba452dea30a805ca8a7a85
Arjun Atapadkar <99797400+arjunatapadkar@users.noreply.github.com>
2024-10-10 09:20:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,7 @@ const App = () => {
           <Route path="*" element={<ErrorPage />} />
         </Routes>        
       </div>
-      <Chat width={200} height={200} />
+      <Chat width={40} height={40} />
     </>
   );
 };
