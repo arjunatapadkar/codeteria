9372e5377c11cef7eddf02013e0be08c59403136
Arjun Atapadkar <99797400+arjunatapadkar@users.noreply.github.com>
2024-10-10 08:53:37 +0530

    Update Chat.jsx

--- src/components/chatbot/Chat.jsx
+++ src/components/chatbot/Chat.jsx
@@ -67,7 +67,7 @@ const Chat = () => {
   }, []);
 
   return (
-    <div className="fixed bottom-4 right-10 z-50">
+    <div className="fixed bottom-4 right-4 z-50">
       <style>{style}</style>
       <WebchatProvider theme={theme} client={client} configuration={config}>
         <Fab onClick={toggleWebchat} />
