11eaf66927018468ffdd6066f7e83c3331fd16b1
Rishi Verma <vrishi7654@gmail.com>
2024-10-11 16:11:30 +0530

    Added Same Foote for all the pages in the Website

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -15,6 +15,8 @@ import logo from "../assets/logo.svg";
 import { Link, useNavigate } from "react-router-dom";
 import { animateScroll as scroll } from "react-scroll";
 import MainNavbar from "../components/MainNavbar";
+import Footer from "../components/core/Footer";
+import Btn from "../components/core/btn";
 import Contact from "../components/Contact";
 
 const codeSnippets = [
@@ -389,89 +391,7 @@ const Homepage = () => {
 
       <Contact />
 
-      <footer className="bg-purple-900 bg-opacity-100 py-6  lg:px-32 backdrop-blur-lg">
-        <div className="container mx-auto px-6">
-          <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-4 py-4">
-            <div>
-              <div className="flex">
-            <img
-              src={logo}
-              alt="Codeteria Logo"
-              className="w-[40px] h-[40px] mr-2"
-            />
-            <Link to="/" className="text-2xl font-bold text-white">
-              Codeteria
-            </Link>
-            </div>
-              <p className="text-gray-300 ml-2">Empowering coders worldwide</p>
-            </div>
-            <div>
-              <h3 className="text-xl font-bold mb-4 text-yellow-400">
-                Quick Links
-              </h3>
-              <ul className="space-y-2">
-                {navs.map(
-                  (item, index) => (
-                    <li key={index}>
-                      <Link
-                        to={item.page}
-                        className="text-gray-300 hover:text-yellow-400 transition-colors"
-                      >
-                        {item.tab}
-                      </Link>
-                    </li>
-                  )
-                )}
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-xl font-bold mb-4 text-yellow-400">
-                Community
-              </h3>
-              <ul className="space-y-2">
-                {["Forums", "Blog", "Events", "Partnerships"].map((item) => (
-                  <li key={item}>
-                    <Link
-                      href="#"
-                      className="text-gray-300 hover:text-yellow-400 transition-colors"
-                    >
-                      {item}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h3 className="text-xl font-bold mb-4 text-yellow-400">
-                Connect
-              </h3>
-              <p className="text-gray-300 mb-3 ml-1">support@codeteria.com</p>
-              <div className="flex space-x-4">
-                {/* Add social media icons here */}
-              </div>
-              <div className="gap-4">
-              <a href="https://facebook.com" target="_blank" className="m-2">
-              <i className="fa-brands fa-facebook p-2 border border-white rounded-full"></i>
-              </a>
-              <a href="https://twitter.com" target="_blank" className="m-2">
-              <i className="fab fa-twitter p-2 border border-white rounded-full"></i>
-              </a>
-              <a href="https://instagram.com" target="_blank" className="m-2">
-              <i className="fab fa-instagram p-2 border border-white rounded-full"></i>
-              </a>
-              <a href="https://linkedin.com" target="_blank" className="m-2">
-              <i className="fab fa-linkedin p-2 border border-white rounded-full"></i>
-              </a>
-              </div>
-            </div>
-            
-          </div>
-          <hr className="border-gray-400" />
-          <div className="mt-4 text-center text-gray-300">
-            © 2024 Codeteria. All rights reserved.
-          </div>
-        </div>
-      </footer>
+      <Footer bg={"#1E2128"} text={"white"} />
       <button
         onClick={scrollToTop}
         className="bg-white opacity-60 text-black  rounded-full h-8 w-8 lg:w-14 lg:h-14 fixed right-6 bottom-24 hover:bg-gray-500 hover:text-white transition duration-300 flex items-center justify-center"
